1741fc119823a8d2025887baeceee136d88ace79
Dimitri75 <bdimitri75@gmail.com>
2015-01-20 22:59:58 +0100

    Ajout classes
    pas testé

---
+++ MyAverage/app/src/main/java/Promotion.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+/**
+ * Created by Dimitri on 20/01/2015.
+ */
+public class Promotion {
+    private String name;
+    private ArrayList<Subject> listSubjects;
+
+    public Promotion(String _name){
+        name = _name;
+    }
+
+    public void setSubject(Subject subject){
+        listSubjects.add(subject);
+    }
+
+    public double getGlobalAverage(){
+        if(listSubjects != null){
+            double sum = 0;
+            int sumCoeff = 0;
+            for (Subject subject : listSubjects){
+                sum += subject.getAverage();
+                sumCoeff += subject.getCoeff();
+            }
+            return sum / sumCoeff;
+        }
+        else return 0;
+    }
+}
---
+++ MyAverage/app/src/main/java/Subject.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+/**
+ * Created by Dimitri on 20/01/2015.
+ */
+public class Subject {
+    private String name;
+    private ArrayList<Double> listMarks;
+    private int coeff;
+
+    public Subject(String _name, int _coeff){
+        name = _name;
+        coeff = _coeff;
+    }
+
+    public void setMark(double mark){
+        listMarks.add(mark);
+    }
+
+    public int getCoeff(){
+        return coeff;
+    }
+
+    public double getAverage(){
+        if (!listMarks.isEmpty()){
+            double sum = 0;
+            for (double mark : listMarks){
+                sum += mark;
+            }
+            return sum / listMarks.size();
+        }
+        else return 0;
+    }
+}
